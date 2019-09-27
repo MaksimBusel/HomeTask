132e3916b7b1112ed9a540cb91d131b3ab35cce7
Maksim <maksimbusel666@gmail.com>
2019-09-27 21:58:40 +0300

    change ArrayCreator

--- src/by/epam/arrays/creator/ArrayCreator.java
+++ src/by/epam/arrays/creator/ArrayCreator.java
@@ -1,23 +1,31 @@
 package by.epam.arrays.creator;
 
+import by.epam.arrays.exception.ExceptionSizeArray;
+
 import java.util.Random;
 
 public class ArrayCreator {
 
     public int[] createArrayNeedSize(int sizeArray) {
-        int[] arrayOrdinarNumbers = new int[sizeArray];
-        for (int i = 0; i < arrayOrdinarNumbers.length; i++) {
-            arrayOrdinarNumbers[i] = i;
+        if (sizeArray > 0) {
+            int[] arrayOrdinarNumbers = new int[sizeArray];
+            for (int i = 0; i < arrayOrdinarNumbers.length; i++) {
+                arrayOrdinarNumbers[i] = i;
+            }
+            return arrayOrdinarNumbers;
         }
-        return arrayOrdinarNumbers;
+        throw new ExceptionSizeArray("Incorrect size array. Size array must be more 0. Try again");
     }
 
     public int[] createArrayRandomNumber(int sizeArray) {
-        Random generateRandomNumbers = new Random();
-        int[] arrayRandomNumbers = new int[sizeArray];
-        for (int i = 0; i < arrayRandomNumbers.length; i++) {
-            arrayRandomNumbers[i] = generateRandomNumbers.nextInt(1000);
+        if (sizeArray > 0) {
+            Random generateRandomNumbers = new Random();
+            int[] arrayRandomNumbers = new int[sizeArray];
+            for (int i = 0; i < arrayRandomNumbers.length; i++) {
+                arrayRandomNumbers[i] = generateRandomNumbers.nextInt(1000);
+            }
+            return arrayRandomNumbers;
         }
-        return arrayRandomNumbers;
+        throw new ExceptionSizeArray("Incorrect size array. Size array must be more 0. Try again");
     }
 }
