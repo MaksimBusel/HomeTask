c3154f0f5791297103c7a30f9c624351746c78a3
Maksim <maksimbusel666@gmail.com>
2019-09-28 17:18:29 +0300

    optimize Validator change RegEx

--- src/by/epam/arrays/validator/Validator.java
+++ src/by/epam/arrays/validator/Validator.java
@@ -6,7 +6,7 @@ public class Validator {
     public String[] checkFileOnValidity(String stringForCheck) {
         String[] arrayForCheck = stringForCheck.split(" ");
         for (int i = 0; i < arrayForCheck.length; i++) {
-            if (!(arrayForCheck[i].matches("^\\d+(\\.\\d+)|\\d+|[+-]\\d+(\\.\\d+)|[+-]\\d+$"))) {
+            if (!(arrayForCheck[i].matches("^[+-]?(\\d+(\\.\\d+)|\\d+)$"))) {
                 return null;
             }
         }
