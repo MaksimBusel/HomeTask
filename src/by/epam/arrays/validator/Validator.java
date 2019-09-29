c1afd994b8eb48b91563947f7da47114d5244047
Maksim <maksimbusel666@gmail.com>
2019-09-29 09:22:02 +0300

    change validator

--- src/by/epam/arrays/validator/Validator.java
+++ src/by/epam/arrays/validator/Validator.java
@@ -4,13 +4,10 @@ package by.epam.arrays.validator;
 public class Validator {
 
     public boolean checkStringOnValidity(String string) {
-        String[] arrayForCheck = string.split(" ");
-        for (int i = 0; i < arrayForCheck.length; i++) {
-            if (!(arrayForCheck[i].matches("^[+-]?\\d+(\\.\\d+)?$"))) {
-                return false;
-            }
+        if (string.matches("^([+-]?\\d+(\\.\\d+)?+\\s?)*$")) {
+            return true;
         }
-        return true;
+        return false;
     }
 }
 
