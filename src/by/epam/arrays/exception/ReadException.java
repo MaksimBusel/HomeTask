3ff2431b819dea8c881300d5a5c6ae64db84f9b4
Maksim <maksimbusel666@gmail.com>
2019-09-30 11:23:56 +0300

    change Exception@

---
+++ src/by/epam/arrays/exception/ReadException.java
@@ -0,0 +1,10 @@
+package by.epam.arrays.exception;
+
+public class ReadException extends RuntimeException {
+    public ReadException(String message) {
+        super(message);
+    }
+
+    public ReadException() {
+    }
+}
